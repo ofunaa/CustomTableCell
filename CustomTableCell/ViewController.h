9e56adb8f2d1a340fe3dbc9befd336d1af1e54e7
takujifunao <takuji.funao@gmail.com>
2015-10-02 15:36:11 +0900

    Initial Commit

---
+++ CustomTableCell/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomTableCell
+//
+//  Created by takuji funao on 2015/10/02.
+//  Copyright (c) 2015年 takuji funao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
